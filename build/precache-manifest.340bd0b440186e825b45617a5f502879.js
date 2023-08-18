b68c038dd0f9ec25a55662de969ce8f2eb9988c7
Saiyam Jain <75769017+Saiyamjain-100@users.noreply.github.com>
2023-08-18 17:32:17 +0530

    Add files via upload

---
+++ build/precache-manifest.340bd0b440186e825b45617a5f502879.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f0f30f8c8a70058d42469f7a005ccbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ea104bbe567e6e2e4cc",
+    "url": "/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "5fce4ef2ec5def04426c",
+    "url": "/static/css/main.7d2118c4.chunk.css"
+  },
+  {
+    "revision": "0ea104bbe567e6e2e4cc",
+    "url": "/static/js/2.b728cd4c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b728cd4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fce4ef2ec5def04426c",
+    "url": "/static/js/main.c5057b01.chunk.js"
+  },
+  {
+    "revision": "350f9b08b1eaaf126d65",
+    "url": "/static/js/runtime-main.82418b2e.js"
+  }
+]);
